53cf438f4c6f827278788a4d21da19110731f839
alexchaoss <45114914+alexchaoss@users.noreply.github.com>
2020-10-17 19:33:47 -0400

    bug

--- src/main/kotlin/com/blizzardarmory/repository/TalentsRepository.kt
+++ src/main/kotlin/com/blizzardarmory/repository/TalentsRepository.kt
@@ -1,12 +1,14 @@
 package com.blizzardarmory.repository
 
+import com.blizzardarmory.model.Achievement
 import com.blizzardarmory.model.Talent
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
 import org.springframework.stereotype.Repository
 
 @Repository
-interface TalentsRepository {
+interface TalentsRepository : JpaRepository<Talent, Long> {
 
     @Query(value = "select t.id," +
             "t.column_index," +
